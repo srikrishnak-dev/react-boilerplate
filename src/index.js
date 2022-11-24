ad7364f3b6c15571c69fa389cdad258313463d50
Sri Krishna <srikrishnak.dev@gmail.com>
2022-11-24 10:54:49 +1000

    final commit to make changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+
+if (process.env.NODE_ENV !== "production") {
+  const axe = require("@axe-core/react");
+  axe(React, createRoot, 1000);
+}
+
 const container = document.getElementById("app");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(<App />);
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,5 +1,12 @@
 const { merge } = require("webpack-merge");
 const BaseConfig = require("./webpack.config.base");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 module.exports = merge(BaseConfig, {
   mode: "production",
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static",
+      reportFilename: "appBundle_Analyzer.html",
+    }),
+  ],
 });
